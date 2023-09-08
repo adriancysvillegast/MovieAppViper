98bb1d8d13787769d23ba7caaf9a85fc819eeb5b
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-09-08 12:52:57 -0400

    Initial Commit

---
+++ TheMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovie
+//
+//  Created by Adriancys Jesus Villegas Toro on 8/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
