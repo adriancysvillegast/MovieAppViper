23a4a07ac4e4ebc9a8ac9f9ced26cd041621e017
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-09-11 21:43:34 -0400

    tabBarAdded and working

--- TheMovie/Moduls/Core/Browser/Router/BrowserRouter.swift
+++ TheMovie/Moduls/Core/Browser/Router/BrowserRouter.swift
@@ -12,28 +12,39 @@ protocol BrowserRouting: AnyObject {
     var browserView : BrowserView? { get }
     var detailView: DetailMovieRouting? { get }
     
-    func showBrowser(window: UIWindow?)
+    func showBrowser() -> UIViewController
     func showDetailMovie(movieID: String)
 }
 
 class BrowserRouter: BrowserRouting {
+    
+    
     // MARK: - Properties
     var browserView : BrowserView?
     var detailView: DetailMovieRouting?
     // MARK: - Methods
-    
-    func showBrowser(window: UIWindow?) {
+    public func showBrowser() -> UIViewController {
         detailView = DetailMovieRouter()
         let interactor = BrowserInteractor()
         let presenter = BrowserPresenter(interactor: interactor, router: self)
-        
-        browserView = BrowserView(presenter: presenter)
-        presenter.view = browserView
+        let view = BrowserView(presenter: presenter)
+        browserView = view
+        presenter.view = view
         interactor.presenter = presenter
-        let view = UINavigationController(rootViewController: browserView!)
-        window?.rootViewController = view
-        window?.makeKeyAndVisible()
+        return view
     }
+    
+//    func showBrowser() {
+//        detailView = DetailMovieRouter()
+//        let interactor = BrowserInteractor()
+//        let presenter = BrowserPresenter(interactor: interactor, router: self)
+//
+//        browserView = BrowserView(presenter: presenter)
+//        presenter.view = browserView
+//        interactor.presenter = presenter
+////        let view = UINavigationController(rootViewController: browserView!)
+//
+//    }
     
     func showDetailMovie(movieID: String) {
         guard let fromViewController = browserView else {
